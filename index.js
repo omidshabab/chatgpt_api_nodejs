94e29573d6f61894188672d1e6acf6b43a911860
Omid Shabab <100057185+omidshababdev@users.noreply.github.com>
2023-06-05 09:15:56 -0700

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const cors = require("cors");
 const { verifyApiKey } = require("./middlewares");
 const router = require("./routes")
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 
 app.use("/v1", verifyApiKey, router)
 
