e118e13d922e2082ce7c98a44d60652d7de426a5
Omid Shabab <100057185+omidshababdev@users.noreply.github.com>
2023-06-05 00:07:05 -0700

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-import express from "express";
-import * as dotenv from "dotenv";
-import cors from "cors";
-import { Configuration, OpenAIApi } from "openai";
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(configuration);
 
 const app = express();
 app.use(cors());
---
+++ middlewares.js
@@ -0,0 +1,22 @@
+const HTTP_STATUS = require("./status");
+
+const verifyApiKey = async (req, res, next) => {
+    try{
+        //
+        let apikey = req.header("Authorization");
+
+        if (!apikey) {
+        return res.status(403).send("Send your OpenAI API Key through header");
+        }
+
+        const verified = 
+        req.user = verified;
+        
+        next()
+    } catch (err) {
+        res.status(500).json({
+            status: HTTP_STATUS.INTERNAL_SERVER_ERROR,
+            data: err.message,
+          });
+    }
+}
