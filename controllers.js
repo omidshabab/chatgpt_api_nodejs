fb0a64c58c62a341b0acf8f3113a253f339c2ff6
Omid Shabab <100057185+omidshababdev@users.noreply.github.com>
2023-06-05 05:46:38 -0700

    first commit

--- controllers.js
+++ controllers.js
@@ -1,7 +1,19 @@
+const request = require("request");
+const HTTP_STATUS = require("./status");
+const dotenv = require("dotenv")
 
+dotenv.config()
+
+// CREATE
 const createCompletion = async (req, res) => {
     try{
-        //
+        const {} = req.query;
+
+        // RESPONSE
+        res.status(200).json({
+            status: HTTP_STATUS.OK,
+            data: response,
+        })
     } catch(err) {
         res.status(500).json({
             status: HTTP_STATUS.INTERNAL_SERVER_ERROR,
@@ -10,6 +22,31 @@ const createCompletion = async (req, res) => {
     }
 }
 
+// READ
+const listModels = async (req, res) => {
+    try{
+        url = `${process.env.OPENAI_API_URL}/models`
+
+        const response = request.post(url)
+
+        // RESPONSE
+        res.status(200).json({
+            status: HTTP_STATUS.OK,
+            data: response,
+        })
+    } catch(err) {
+        res.status(500).json({
+            status: HTTP_STATUS.INTERNAL_SERVER_ERROR,
+            data: err.message,
+          });
+    }
+}
+
+// UPDATE
+
+// DELETE
+
 module.exports = {
     createCompletion,
+    listModels,
 }
--- routes.js
+++ routes.js
@@ -1,8 +1,27 @@
 const express = require("express")
-const { createCompletion } = require("./controllers")
+const { 
+    // CREATE
+    createCompletion,
+    
+    // READ
+    listModels,
+
+    // UPDATE
+
+    // DELETE
+
+} = require("./controllers")
 
 const router = express.Router()
 
+// CREATE
 router.post("/completions", createCompletion)
+
+// READ
+router.get("/models", listModels)
+
+// UPDATE
+
+// DELETE
 
 module.exports = router
